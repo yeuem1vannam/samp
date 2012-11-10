3c92954153d6970430374684291e299f0bd8808f
PhuongLH <le.hoai.phuong@framgia.com>
2012-11-10 10:45:28 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samp::Application.config.secret_token = 'fafc5a95a1893c021177778171bfbb3ed68780a3e88e054fafed1d181cdaf9a209a82d028fd476b5aaac6be8ea51e6c34efc25402848975d5dbe219f5ad9a9e8'
